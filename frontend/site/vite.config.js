7a68007832c6167ccb70e201c9e5f05c85e340ce
DuKaJT03 <jhonticona2001@gmail.com>
2025-10-07 14:37:22 -0300

    Removendo node_modules e ajustando estrutura para deploy

--- frontend/site/vite.config.js
+++ frontend/site/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portifolio_jhon', // nome do repositório Github
+  build: {
+    outDir: 'dist', //pasta de saída do build
+  },
 })
