4260461af23ac7570a9a6bbb4b4dba2060389b91
DuKaJT03 <jhonticona2001@gmail.com>
2025-10-30 19:37:23 -0300

    Corrige base no vite.config para deploy a adiciona package-lock

--- frontend/site/vite.config.js
+++ frontend/site/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portifolio_jhon', // nome do repositório Github
+  base: '/portifolio_jhon/', // nome do repositório Github
   build: {
     outDir: 'dist', //pasta de saída do build
   },
